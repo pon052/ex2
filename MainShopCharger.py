bdaafc4115978ce4b0c4a2fbb54b8807d8418e31
Kenta Fujisawa <kentanogotokyu@gmail.com>
2019-01-07 22:57:08 +0900

    [modified]toriaezu dekita.

--- MainShopCharger.py
+++ MainShopCharger.py
@@ -41,11 +41,25 @@ class MainShopCharger:
         self.print_account_balance()
         print("------------------")
 
+    def most_similar(self):
+        if(self.inserted_student_card is not None):
+            text = self.inserted_student_card.get_text()
+            tokens = text.split("/")
+            vectors = []
+            for token in tokens:
+                vectors.append(token)
+            print("関連ワード :")
+            results = MainShopCharger.model.most_similar(positive = vectors,negative = [],topn=5)
+            for result in results:
+                print(str(result[0]) + " : " + str(result[1]))
+        else:
+            print("学生証が挿入されていません．")
+
     def main(self):
         # Main Sentences
         # 学生証インスタンスの作成
-        student_card_1 = StudentCard(0, "tut","仁")
-        student_card_2 = StudentCard(1, "tempaku","義")
+        student_card_1 = StudentCard(0, "tut","とんかつ/からあげ/エビフライ")
+        student_card_2 = StudentCard(1, "tempaku","内田真礼/水瀬いのり/佐倉綾音/佐藤聡美/種田梨沙")
 
         # 初期残高の設定
         student_card_1.set_balance(1000)
@@ -56,11 +70,12 @@ class MainShopCharger:
         # 学生証１枚目のカードをセット
         self.insert_card(0)
 
+        self.most_similar()
         # 加算
         self.charge_money(1000)
 
         # 減算
-        self.charge_money(-300)
+        #self.charge_money(-300)
 
         # 学生証の２枚目の挿入とチャージ
         self.insert_card(1)
@@ -69,10 +84,9 @@ class MainShopCharger:
         self.charge_money(500)
 
         # 引き出し
-        self.charge_money(-100)
+        #self.charge_money(-100)
 
-        result = MainShopCharger.model.most_similar(positive=['東京','フランス'],negative=['日本'])
-        print(result)
+        self.most_similar()
 
 if __name__ == '__main__':
     Charger = MainShopCharger()
